34b3c40585f125be41167a2168873706d368844d
Nicolás González <nicolook.pro@gmail.com>
2024-05-07 15:00:22 -0300

    Add Site to Astro Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
 
   // Site y Base son para colocar un Build en producción - En este caso también configurar HOME_URL en .env
   // Aqui va el dominio
-  // site: 'https://nico-software-update.netlify.app',
+  site: 'https://nico-quotes.netlify.app',
   // site: 'http://localhost:4321',
 
   // Aqui va el folder, en caso de root -> /
