3fda6454532520fb5776f4c6e854afddf0a47ec1
Nicolás González <nicolook.pro@gmail.com>
2024-07-09 17:36:59 -0300

    Improve decodeHTMLtxt

--- src/js/decodeHTMLtxt.js
+++ src/js/decodeHTMLtxt.js
@@ -1,7 +1,27 @@
 function decodeHTMLtxt(text) {
-    const textArea = document.createElement('textarea');
-    textArea.innerHTML = text;
-    return textArea.value;
+    const entities = {
+        'amp': '&',
+        'lt': '<',
+        'gt': '>',
+        'quot': '"',
+        'apos': "'",
+        '#8211': '–',
+        // Añade más entidades según sea necesario
+    };
+
+    return text.replace(/&(#?[\w\d]+);/g, (match, entity) => {
+        if (entity in entities) {
+            return entities[entity];
+        } else if (entity.charAt(0) === '#') {
+            const codePoint = entity.charAt(1).toLowerCase() === 'x' ?
+                parseInt(entity.substr(2), 16) :
+                parseInt(entity.substr(1), 10);
+            if (!isNaN(codePoint)) {
+                return String.fromCodePoint(codePoint);
+            }
+        }
+        return match;
+    });
 }
 
 export default decodeHTMLtxt
